e7ce82f10824a5e2b62b0e554cf6b8177d01a6d5
Ho Thien Ai <thienaiho95@gmail.com>
2026-04-26 14:49:58 +0700

    Recursive function

---
+++ Recursive_Function/Core/Src/bsp.h
@@ -0,0 +1,24 @@
+/*
+ * bsp.h
+ *
+ *  Created on: Apr 10, 2026
+ *      Author: Ai Ho
+ */
+
+#ifndef BSP_H_
+#define BSP_H_
+
+#include <stdint.h>
+#include "stm32f1xx_hal.h"
+
+void BSP_turnObLedOn(void);
+void BSP_turnObLedOff(void);
+void BSP_toggleObLed(void);
+
+GPIO_PinState BSP_readObButton(void);
+
+void BSP_delayMs(uint32_t delay);
+
+uint32_t BSP_getTickMs(void);
+
+#endif /* BSP_H_ */
